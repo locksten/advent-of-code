d5a8822e71a13e2b4e2d422691c0fd0a5f4cc799
locksten <email@locksten.com>
2022-12-04 17:59:43 +0200

    Day 4

---
+++ 2022/04_Camp_Cleanup/2.ts
@@ -0,0 +1,16 @@
+const input = await Deno.readTextFile("./input.txt");
+
+const pairs = input
+  .split("\n")
+  .map((pair) => pair.split(",").map((range) => range.split("-").map(Number)));
+
+const containsCount = pairs.filter(
+  ([[a1, a2], [b1, b2]]) =>
+    (a1 >= b1 && a1 <= b2) ||
+    (a2 >= b1 && a2 <= b2) ||
+    (a1 <= b1 && a2 >= b2) ||
+    (a1 >= b1 && a2 <= b2)
+).length;
+
+console.log(pairs);
+console.log(containsCount);
